fc93b7f95d29dc4f753431b37d2bf0e4252db058
Arthur Lucas Costa Ciriaco da Silva <arthur.lucas.127@ufrn.edu.br>
2025-10-05 16:20:44 -0300

    Atualização BabyNat

--- Solutions/FMCBabyNat.hs
+++ Solutions/FMCBabyNat.hs
@@ -1,7 +1,7 @@
 module FMCBabyNat where
 
 -- Do not alter this import!
-import Prelude ( Show(..) , Eq(..) , undefined )
+import Prelude (Eq (..), Num (negate), Show (..), undefined)
 
 -- Define evenerything that is undefined,
 -- without using standard Haskell functions.
@@ -11,67 +11,83 @@ import Prelude ( Show(..) , Eq(..) , undefined )
 data Nat = O | S Nat
   deriving (Eq, Show)
 
--- some sugar
+-- some sugar(OK)
 zero, one, two, three, four, five, six, seven, eight :: Nat
-zero  = O
-one   = S zero
-two   = S one
+zero = O
+one = S zero
+two = S one
 three = S two
-four  = S three
-five  = S four
-six   = S five
+four = S three
+five = S four
+six = S five
 seven = S six
 eight = S seven
 
--- addition
+-- addition (OK)
 (+) :: Nat -> Nat -> Nat
-n + O   = n
+n + O = n
 n + S m = S (n + m)
 
- -- syntactic associativity: L
- -- syntactic precedence: 6
+-- syntactic associativity: L
+-- syntactic precedence: 6
 infixl 6 +
 
--- Output: O means False, S O means True
+-- Output: O means False, S O means True (FALTA)
 isZero :: Nat -> Nat
-isZero = undefined
+isZero O = S O
+isZero (S n) = O
 
--- pred is the predecessor but we define zero's to be zero
+-- pred is the predecessor but we define zero's to be zero (FALTA)
 pred :: Nat -> Nat
-pred = undefined
+pred O = O
+pred (S n) = n
 
--- Output: O means False, S O means True
+-- Output: O means False, S O means True(FALTA)
 even :: Nat -> Nat
-even = undefined
+even O = S O
+even (S O) = O
+even (S (S n)) = even n -- Conserva a paridade no
 
 odd :: Nat -> Nat
-odd = undefined
+odd n = isZero (even n) -- retorna o isZero do even de n, ou seja se even n for verdadeiro (S O), ele inverte, ou seja o odd é O;
 
 -- This is called the dotminus or monus operator
 -- (also: proper subtraction, arithmetic subtraction, ...).
 -- It behaves like subtraction, except that it returns 0
 -- when "normal" subtraction would return a negative number.
-monus :: Nat -> Nat -> Nat
-monus = undefined
+-- separar por casos
+monus n O = n -- caso base de n - 0 = n
+monus O m = O -- de acordo com o enunciado (caso base)
+monus (S n) (S m) = monus n m
 
 (-*) :: Nat -> Nat -> Nat
 (-*) = monus
 
 -- multiplication
 (*) :: Nat -> Nat -> Nat
-(*) = undefined
+n * O = O -- caso base
+n * m = n + (n * pred m)
 
 infixl 7 *
 
 -- exponentiation
 (^) :: Nat -> Nat -> Nat
-(^) = undefined
+_ ^ O = one
+O ^ _ = O
+n ^ m = n * (n ^ pred m)
 
--- decide: infix? ? ^
+infix 8 ^
 
 -- quotient
 (/) :: Nat -> Nat -> Nat
-(/) = undefined
+n / O = O
+O / m = O
+n / m =
+  case n -* S m of
+    O -> O
+    rest -> S (rest / S m)
+
+infix 9 /
 
 -- remainder
 (%) :: Nat -> Nat -> Nat
@@ -102,4 +118,3 @@ sg = undefined
 -- lo b a is the floor of the logarithm base b of a
 lo :: Nat -> Nat -> Nat
 lo = undefined
-
